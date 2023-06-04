9d45861550152be0ec84ce79c29c0435c2ccd369
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-06-04 10:44:00 -0400

    0.0.37

--- be-a-beacon.js
+++ be-a-beacon.js
@@ -1,18 +1,18 @@
 import { BE, propDefaults, propInfo } from 'be-enhanced/BE.js';
 import { XE } from 'xtal-element/XE.js';
 import { register } from 'be-hive/register.js';
-export class BeABeacon2 extends BE {
-    async attach(enhancedElement, enhancement) {
+export class BeABeacon extends BE {
+    async attach(enhancedElement, enhancementInfo) {
         enhancedElement.dispatchEvent(new CustomEvent('i-am-here', {
             bubbles: true,
         }));
-        await super.attach(enhancedElement, enhancement);
+        await super.attach(enhancedElement, enhancementInfo);
         this.resolved = true;
     }
 }
 const tagName = 'be-a-beacon';
 const ifWantsToBe = 'a-beacon';
-const upgrade = 'template';
+const upgrade = '*';
 const xe = new XE({
     config: {
         tagName,
@@ -23,6 +23,6 @@ const xe = new XE({
             ...propInfo
         },
     },
-    superclass: BeABeacon2
+    superclass: BeABeacon
 });
 register(ifWantsToBe, upgrade, tagName);
--- be-a-beacon.ts
+++ be-a-beacon.ts
@@ -1,22 +1,22 @@
 import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
 import {XE} from 'xtal-element/XE.js';
-import {IBE} from 'be-enhanced/types.js';
+import {IBE, EnhancementInfo} from 'be-enhanced/types.js';
 import {Actions} from './types';
 import {register} from 'be-hive/register.js';
 
 export class BeABeacon extends BE{
-    override async attach(enhancedElement: Element, enhancement: string): Promise<void> {
+    override async attach(enhancedElement: Element, enhancementInfo: EnhancementInfo): Promise<void> {
         enhancedElement.dispatchEvent(new CustomEvent('i-am-here', {
             bubbles: true,
         }));
-        await super.attach(enhancedElement, enhancement);
+        await super.attach(enhancedElement, enhancementInfo);
         this.resolved = true;
     }
 }
 
 const tagName = 'be-a-beacon';
 const ifWantsToBe = 'a-beacon';
-const upgrade = 'template';
+const upgrade = '*';
 
 const xe = new XE<IBE, Actions>({
     config: {
