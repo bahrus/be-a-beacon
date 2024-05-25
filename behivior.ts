9390d0db685e5638bf6029ab45166e8fe0d00efb
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-05-25 09:05:31 -0400

    0.0.53

--- behivior.ts
+++ behivior.ts
@@ -1,22 +1,26 @@
-
-
-
 import {BeHive} from 'be-hive/be-hive.js';
-
 import './behance.js';
+import {EnhancementMountCnfg} from 'trans-render/be/types';
+import {MountObserverScriptElement} from 'mount-observer/types';
+import {MountObserver} from 'mount-observer/MountObserver.js';
 
-BeHive.registry.register({
+const emc: EnhancementMountCnfg = {
     base: 'be-a-beacon',
     map: {
         '0.0': 'eventName'
     },
-    do:{
-        mount:{
-            import: async () => {
-                const {BeABeacon} = await import('./be-a-beacon.js');
-                return BeABeacon;
-            }
-        }
+    enhPropKey: 'beABeacon',
+    importEnh: async () => {
+        const {BeABeacon} = await import('./be-a-beacon.js');
+        return BeABeacon;
     }
-});
+}
+
+const mose = document.createElement('script') as MountObserverScriptElement<EnhancementMountCnfg>;
+mose.type = 'mountobserver';
+mose.synConfig = emc;
+
+MountObserver.synthesize(document, BeHive, mose);
+
+
 
