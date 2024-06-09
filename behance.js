5e27392af8c0e5f6d0163c9dd6bcb0a24bdf14ab
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-06-09 17:30:12 -0400

    0.0.60

--- behance.js
+++ behance.js
@@ -1,5 +1,7 @@
 import { BeABeacon } from './be-a-beacon.js';
 export { BeABeacon } from './be-a-beacon.js';
 import { def } from 'trans-render/lib/def.js';
+import { emc } from './behivior.js';
+BeABeacon['synConfig'] = emc;
 await BeABeacon.bootUp();
 def('be-a-beacon', BeABeacon);
--- behance.ts
+++ behance.ts
@@ -1,7 +1,9 @@
 import {BeABeacon} from './be-a-beacon.js';
 export {BeABeacon} from './be-a-beacon.js';
 import {def} from 'trans-render/lib/def.js';
+import {emc} from './behivior.js';
 
+(<any>BeABeacon)['synConfig'] = emc;
 await BeABeacon.bootUp();
 
 def('be-a-beacon', BeABeacon);
