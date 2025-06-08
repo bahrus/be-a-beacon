d88943310c943822dbb43b4b20856d5cfddf5786
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-08 14:32:27 -0400

    0.0.73

--- emc.js
+++ emc.js
@@ -1,9 +1,10 @@
 // @ts-check
 import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
 /** @import {EMC} from './ts-refs/trans-render/be/types' */
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-a-beacon/types' */;
 
 /**
- * @type {EMC}
+ * @type {EMC<any, AP>}
  */
 export const emc = {
     base: 'be-a-beacon',
